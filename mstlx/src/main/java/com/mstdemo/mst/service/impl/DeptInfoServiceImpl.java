fa7cc3bbb35e68280f37250d5bdfe29d0ab14f60
gitxwhl <githubjava@163.com>
2022-08-21 15:13:05 +0800

    # 遍历年份之间的月份信息

--- mstlx/src/main/java/com/mstdemo/mst/service/impl/DeptInfoServiceImpl.java
+++ mstlx/src/main/java/com/mstdemo/mst/service/impl/DeptInfoServiceImpl.java
@@ -4,9 +4,16 @@ import com.mstdemo.mst.bean.DeptInfo;
 import com.mstdemo.mst.bean.Permission;
 import com.mstdemo.mst.mapper.DeptInfoMapper;
 import com.mstdemo.mst.service.DeptInfoService;
+import com.mstdemo.mst.util.StreamZipUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.sql.SQLOutput;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 @Service
 public class DeptInfoServiceImpl implements DeptInfoService {
@@ -52,4 +59,61 @@ public class DeptInfoServiceImpl implements DeptInfoService {
         }
         parent.setChildren(childPermissions);
     }
+
+    /**
+     * 处理时间循环遍历出现数组越界
+     * @param
+     * @param
+     */
+    public static void getTime(String starttime, String endtime){
+       int starttimey = Integer.valueOf(starttime.substring(0,4));
+       int starttimem = Integer.valueOf(starttime.substring(5));
+       int endtimey = Integer.valueOf(endtime.substring(0,4));
+       int endtimem = Integer.valueOf(endtime.substring(5));
+       int l = 0;
+       String xAxis [] = new String[10000];
+       for (int i=0;starttimey <= endtimey;i++){
+           if(starttimey == endtimey && starttimem <= endtimem){
+               if(starttimem < 10){
+                   xAxis[l] = starttimey + "-0" + starttimem;
+                   starttimem = starttimem+1;
+                   System.out.println(xAxis[l]);
+                   l= l+1;
+               }else if(starttimem < 13 && starttimem >= 10){
+
+                   xAxis[l] = starttimey + "-" + starttimem;
+                   starttimem = starttimem+1;
+                   System.out.println(xAxis[l]);
+                   l= l+1;
+               }
+           }
+
+           if(starttimey < endtimey)
+            {
+               if(starttimem < 10){
+                   xAxis[l] = starttimey + "-0" + starttimem;
+                   starttimem = starttimem+1;
+                   System.out.println(xAxis[l]);
+                   l= l+1;
+               }else if(starttimem < 13 && starttimem >= 10){
+                   xAxis[l] = starttimey + "-" + starttimem;
+                   starttimem = starttimem+1;
+                   System.out.println(xAxis[l]);
+                   l= l+1;
+               }else {
+                   starttimey= starttimey+1;
+                   starttimem = starttimem-12;
+               }
+           }
+       }
+    }
+
+
+    public static void main(String[] args) {
+        //日期之间的调用
+        getTime("1993-08" ,"2022-11");
+        //解压测试
+//        decompression("D:\\sss.zip","D:\\xjzh\\");
+    }
+
 }
