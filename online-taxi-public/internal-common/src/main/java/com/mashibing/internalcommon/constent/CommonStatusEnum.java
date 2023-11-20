c7b886142beb7263c5dd0097f349682b7a46fb71
gitxwhl <githubjava@163.com>
2023-11-20 11:42:32 +0800

    统一返回格式，制定

---
+++ online-taxi-public/internal-common/src/main/java/com/mashibing/internalcommon/constent/CommonStatusEnum.java
@@ -0,0 +1,22 @@
+package com.mashibing.internalcommon.constent;
+
+import lombok.Data;
+import lombok.Getter;
+
+/**
+ * 固定的状态码
+ * 固定的对象用枚举,枚举类要写在最前面
+ */
+public enum CommonStatusEnum {
+    SUCESS(1,"sucess"),
+    FAIL(0,"fail");
+    @Getter
+    private int code;
+    @Getter
+    private String value;
+
+    CommonStatusEnum(int code, String value) {
+        this.code = code;
+        this.value = value;
+    }
+}
