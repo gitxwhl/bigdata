ddbb12836ae9305c798e1411d8e172386ead8926
gitxwhl <githubjava@163.com>
2023-12-22 18:15:34 +0800

    测试token   controller  编写

--- online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/controller/TestController.java
+++ online-taxi-public/api-passenger/src/main/java/com/mashibing/apipassenger/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.mashibing.apipassenger.controller;
 
+import com.mashibing.internalcommon.dto.ResponseResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,25 @@ public class TestController {
     public String getTest(){
         return "测试";
     }
+
+    /**
+     * 需要token
+     * @return
+     */
+    public ResponseResult authTest(){
+
+        return ResponseResult.success("auth success");
+    }
+
+    /**
+     * 没有token正常返回
+     * @return
+     */
+    public ResponseResult noauthTest(){
+
+        return ResponseResult.success("noauth  test");
+    }
+
 
 
 }
