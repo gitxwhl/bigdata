2d83b9ec9b7855e575bc3d9f428d985d4997f268
gitxwhl <githubjava@163.com>
2024-01-23 16:08:52 +0800

    spring boot  项目相对路径单文件上传

--- springboot-fileupload/src/main/java/com/msb/controller/FileController.java
+++ springboot-fileupload/src/main/java/com/msb/controller/FileController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -49,6 +50,56 @@ public class FileController {
         map.put("filetype", photo.getContentType());
         return map;
     }*/
+
+    /**
+     * 项目相对路径：单文件上传
+     * @param headPhoto
+     * @param req
+     * @return
+     * @throws IOException
+     */
+    @ResponseBody
+    @RequestMapping("/uploadone")
+    public Map<String,String> fileUpload01(MultipartFile headPhoto,HttpServletRequest req) throws IOException {
+        Map<String,String> map=new HashMap<String,String>();
+        //指定文件存储目录
+//        File dir=new File("d:/images");
+//        指定文件存储目录为我们项目部署环境下upload目录
+        String realPath = req.getServletContext().getRealPath("/upload");
+        File dir = new File(realPath);
+        System.out.println(realPath);
+        //如果不存在创建目录
+        if (!dir.exists()){
+            dir.mkdir();
+        }
+        //获取文件名
+        String filename = headPhoto.getOriginalFilename();
+        //避免文件名冲突，使用uuid
+        String nameUid= UUID.randomUUID().toString();
+        //截取文件名后缀
+        String sufix = filename.substring(filename.lastIndexOf("."));
+        System.out.println(sufix);
+        if(!sufix.equals(".jpg")){
+            map.put("message","文件类型错误");
+            return map;
+        }
+        //组合新的文件名
+        String newFileName = nameUid+sufix;
+        //指定文件存储位置
+        File file = new File(dir,newFileName);
+        //文件保存
+        headPhoto.transferTo(file);
+        //上传成功返回文件名和文件的类型返回给浏览器
+        map.put("message","文件上传成功");
+        map.put("newFileName",newFileName);
+        map.put("sufix",headPhoto.getContentType());
+        return map;
+    }
+
+
+
+
+
 
     /**
      * 跨服务器的单文件文件上传
