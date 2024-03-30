e4f956da34511c71b13b82fe692053223d7c61a9
gitxwhl <githubjava@163.com>
2024-03-30 21:48:46 +0800

    service-drive-user 注册到nacos

--- online-taxi-public/service-drive-user/src/main/java/com/mashibing/driveuser/ServiceDriveUserApplication.java
+++ online-taxi-public/service-drive-user/src/main/java/com/mashibing/driveuser/ServiceDriveUserApplication.java
@@ -3,9 +3,11 @@ package com.mashibing.driveuser;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.mashibing.driveuser.mapper")
+@EnableDiscoveryClient
 public class ServiceDriveUserApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceDriveUserApplication.class);
