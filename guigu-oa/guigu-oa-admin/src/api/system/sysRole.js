96533fc6235c981c3256fd7b9696bdc4a51d8c9f
gitxwhl <githubjava@163.com>
2024-06-20 21:45:30 +0800

    前端vue，js结构

---
+++ guigu-oa/guigu-oa-admin/src/api/system/sysRole.js
@@ -0,0 +1,15 @@
+import request from '@/utils/request'
+// 统一抽取
+const api_name = '/admin/system/sysRole'
+export default {
+/* 角色列表条件分页查询 */
+  getPageList(current, limit, searchObj) {
+    return request({
+      url: `${api_name}/${current}/${limit}`,
+      method: 'get',
+      /* 如果是普通对象参数写法，params：对象参数名称
+      如果是json对象参数写法，data：对象参数名称 */
+      params: searchObj
+    })
+  }
+}
---
+++ guigu-oa/guigu-oa-admin/src/views/system/sysRole/list.vue
@@ -0,0 +1,26 @@
+<script>
+/* 引入定义接口的js文件 */
+import api from '@/api/system/sysRole.js'
+export default {
+  //vue 代码结构
+  //初始值
+  data() {
+    return{
+
+    }
+    },
+    created() {  //在渲染之前執行
+    this.fetchData();
+    },
+  methods: function() {  //操作方法
+    //条件分页查询方法
+    fetchData()
+    {
+
+    }
+
+  }
+  }
+
+
+</script>
