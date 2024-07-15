4b34d9d455fbf5d1653ebca18f030616b34d61df
gitxwhl <githubjava@163.com>
2024-07-15 08:48:48 +0800

    云尚系统，1：登录filter 判断用户名密码是否正确，生成token 2：认证token组件，判断请求头是否有token，如果有认证完成

---
+++ guigu-oa-parent/common/spring-security/src/main/java/com/atguigu/security/filter/TokenAuthenticationFilter.java
@@ -0,0 +1,63 @@
+package com.atguigu.security.filter;
+
+import com.atguigu.common.result.ResponseUtil;
+import com.atguigu.common.result.Result;
+import com.atguigu.common.result.ResultCodeEnum;
+import com.atguigu.jwt.JwtHelper;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
+/**
+ * <p>
+ * 认证解析token过滤器
+ * </p>
+ */
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
+    public TokenAuthenticationFilter() {
+
+    }
+//执行各接口的访问首先进入过滤器
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        logger.info("uri:"+request.getRequestURI());
+        //如果是登录接口，直接放行
+        if("/admin/system/index/login".equals(request.getRequestURI())) {
+            chain.doFilter(request, response);
+            return;
+        }
+        //如果不是登录接口，判断请求头是否有token
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
+        if(null != authentication) {
+            //如果不等于空，放到安全上下文对象中
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            chain.doFilter(request, response);
+        } else {
+            ResponseUtil.out(response, Result.build(null, ResultCodeEnum.LOGIN_ERROR));
+        }
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+        // token置于header里  请求头是否有token
+        String token = request.getHeader("token");
+        logger.info("token:"+token);
+        if (!StringUtils.isEmpty(token)) {
+            String useruame = JwtHelper.getUsername(token);
+            logger.info("useruame:"+useruame);
+            if (!StringUtils.isEmpty(useruame)) {
+                return new UsernamePasswordAuthenticationToken(useruame, null, Collections.emptyList());
+            }
+        }
+        return null;
+    }
+}
---
+++ guigu-oa-parent/common/spring-security/src/main/java/com/atguigu/security/filter/TokenLoginFiler.java
@@ -0,0 +1,100 @@
+package com.atguigu.security.filter;
+
+import atguigu.vo.system.LoginVo;
+import com.atguigu.common.result.ResponseUtil;
+import com.atguigu.common.result.Result;
+import com.atguigu.common.result.ResultCodeEnum;
+import com.atguigu.jwt.JwtHelper;
+import com.atguigu.security.custom.CustomUser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+//登录filter,判断用户名和密码是否正确，生成token
+public class TokenLoginFiler extends UsernamePasswordAuthenticationFilter {
+//        构造方法
+    public TokenLoginFiler(AuthenticationManager authenticationManager) {
+        this.setAuthenticationManager(authenticationManager);
+//        提交方式
+        this.setPostOnly(false);
+        //指定登录接口及提交方式，可以指定任意路径
+        this.setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/admin/system/index/login","POST"));
+    }
+
+    /**
+     * 登录认证
+     * 得到用户名和密码封装成 Authentication 对象 完成认证
+     * @param req
+     * @param res
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res)
+            throws AuthenticationException {
+        try {
+            //获取用户信息
+            LoginVo loginVo = new ObjectMapper().readValue(req.getInputStream(), LoginVo.class);
+            //封装对象
+            Authentication authenticationToken = new UsernamePasswordAuthenticationToken(loginVo.getUsername(), loginVo.getPassword());
+            //调用方法完成认证
+            return this.getAuthenticationManager().authenticate(authenticationToken);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /**
+     * 登录成功 认证成功
+     * @param request
+     * @param response
+     * @param chain
+     * @param auth
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication auth) throws IOException, ServletException {
+
+        //获取认证对象（当前用户对象）
+        CustomUser customUser = (CustomUser) auth.getPrincipal();
+        //生成token
+        String token = JwtHelper.createToken(customUser.getSysUser().getId(), customUser.getSysUser().getUsername());
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("token", token);
+        ResponseUtil.out(response, Result.ok(map));
+    }
+
+    /**
+     * 登录失败  认证失败
+     * @param request
+     * @param response
+     * @param e
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                              AuthenticationException e) throws IOException, ServletException {
+
+            ResponseUtil.out(response, Result.build(null, ResultCodeEnum.LOGIN_ERROR));
+
+    }
+
+
+}
