46ec2c35544fa286cff55dec97c33a4ed293720c
gitxwhl <githubjava@163.com>
2024-07-16 19:31:45 +0800

    云尚系统，spring security 授权：3：修改TokenAuthenticationFilter,从请求头获取token，获取username 拿着username到redis查询权限数据

--- guigu-oa-parent/common/spring-security/src/main/java/com/atguigu/security/filter/TokenAuthenticationFilter.java
+++ guigu-oa-parent/common/spring-security/src/main/java/com/atguigu/security/filter/TokenAuthenticationFilter.java
@@ -1,27 +1,42 @@
 package com.atguigu.security.filter;
 
+import com.alibaba.fastjson.JSON;
 import com.atguigu.common.result.ResponseUtil;
 import com.atguigu.common.result.Result;
 import com.atguigu.common.result.ResultCodeEnum;
 import com.atguigu.jwt.JwtHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
  * 认证解析token过滤器
  * </p>
  */
+@Component
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+    public TokenAuthenticationFilter(RedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
 
     public TokenAuthenticationFilter() {
 
@@ -55,7 +70,22 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
             String useruame = JwtHelper.getUsername(token);
             logger.info("useruame:"+useruame);
             if (!StringUtils.isEmpty(useruame)) {
-                return new UsernamePasswordAuthenticationToken(useruame, null, Collections.emptyList());
+                //通过username从redis获取权限的数据
+                String authString = (String) redisTemplate.opsForValue().get(useruame);
+                //把redis获取的字符串权限数据转换成要求集合数据 List<SimpleGrantedAuthority>
+
+                if(!StringUtils.isEmpty(authString)){
+                    List<Map> mapList = JSON.parseArray(authString, Map.class);
+                    List<SimpleGrantedAuthority> authList = new ArrayList<>();
+                    for (Map map:mapList){
+                        String authority = (String) map.get("authority");
+                        authList.add(new SimpleGrantedAuthority(authority));
+                    }
+                    return new UsernamePasswordAuthenticationToken(useruame, null,authList);
+                }else{
+                    return new UsernamePasswordAuthenticationToken(useruame, null,new ArrayList<>());
+                }
+
             }
         }
         return null;
