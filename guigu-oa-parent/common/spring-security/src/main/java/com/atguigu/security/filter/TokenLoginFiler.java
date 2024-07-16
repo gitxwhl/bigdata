2e0838fa1d242001a7c271fce739571c71b5af3c
gitxwhl <githubjava@163.com>
2024-07-16 19:30:34 +0800

    云尚系统，spring security 授权：,2:修改TokenLoginFilter,增加权限数据部分，获取当前的权限数据，把权限数据放入redis，

--- guigu-oa-parent/common/spring-security/src/main/java/com/atguigu/security/filter/TokenLoginFiler.java
+++ guigu-oa-parent/common/spring-security/src/main/java/com/atguigu/security/filter/TokenLoginFiler.java
@@ -1,19 +1,20 @@
 package com.atguigu.security.filter;
 
 import atguigu.vo.system.LoginVo;
+import com.alibaba.fastjson.JSON;
 import com.atguigu.common.result.ResponseUtil;
 import com.atguigu.common.result.Result;
 import com.atguigu.common.result.ResultCodeEnum;
 import com.atguigu.jwt.JwtHelper;
 import com.atguigu.security.custom.CustomUser;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -24,13 +25,20 @@ import java.util.Map;
 
 //登录filter,判断用户名和密码是否正确，生成token
 public class TokenLoginFiler extends UsernamePasswordAuthenticationFilter {
-//        构造方法
-    public TokenLoginFiler(AuthenticationManager authenticationManager) {
+
+    private RedisTemplate redisTemplate;
+
+
+    //        构造方法
+    public TokenLoginFiler(AuthenticationManager authenticationManager,
+                        RedisTemplate redisTemplate
+                        ) {
         this.setAuthenticationManager(authenticationManager);
 //        提交方式
         this.setPostOnly(false);
         //指定登录接口及提交方式，可以指定任意路径
         this.setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/admin/system/index/login","POST"));
+        this.redisTemplate=redisTemplate;
     }
 
     /**
@@ -74,6 +82,8 @@ public class TokenLoginFiler extends UsernamePasswordAuthenticationFilter {
         CustomUser customUser = (CustomUser) auth.getPrincipal();
         //生成token
         String token = JwtHelper.createToken(customUser.getSysUser().getId(), customUser.getSysUser().getUsername());
+        //获取当前用户权限数据，放到redis中：key：username    value  权限数据
+        redisTemplate.opsForValue().set(customUser.getUsername(), JSON.toJSONString(customUser.getAuthorities()));
 
         Map<String, Object> map = new HashMap<>();
         map.put("token", token);
