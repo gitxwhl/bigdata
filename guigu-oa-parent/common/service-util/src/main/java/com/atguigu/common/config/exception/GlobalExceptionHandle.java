e85ec7906a57a2da5e791e658bc7964f26582e2e
gitxwhl <githubjava@163.com>
2024-07-16 19:59:04 +0800

    云尚系统，spring security 授权：添加 按钮没有操作权限 全局异常处理，注意：需要导入security 包

--- guigu-oa-parent/common/service-util/src/main/java/com/atguigu/common/config/exception/GlobalExceptionHandle.java
+++ guigu-oa-parent/common/service-util/src/main/java/com/atguigu/common/config/exception/GlobalExceptionHandle.java
@@ -1,9 +1,12 @@
 package com.atguigu.common.config.exception;
 
 import com.atguigu.common.result.Result;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+
 
 @ControllerAdvice
 public class GlobalExceptionHandle {
@@ -22,12 +25,18 @@ public class GlobalExceptionHandle {
         e.printStackTrace();
         return Result.fail().message("执行了特定异常处理。。。。。。。");
     }
+
     @ExceptionHandler(GuiguException.class)
     @ResponseBody
     public Result error(GuiguException e){
         e.printStackTrace();
         return Result.fail().message(e.getMsg()).code(e.getCode());
     }
-
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseBody
+    public Result error(AccessDeniedException e) {
+        e.printStackTrace();
+        return Result.fail().code(205).message("没有操作权限");
+    }
 
 }
